e2f4cbed0c51c734b99234f5fbb69934f0aae7cc
Abhisek Rath <iamabhisekrath@gmail.com>
2023-04-27 22:56:06 +0530

    Add files via upload

---
+++ pages/gigaweb.tsx
@@ -0,0 +1,19 @@
+import Resource from "../components/resources/Resource";
+import Layout from "../components/Layout";
+import Head from "next/head";
+import GigaWebContent from "../components/gigaweb/content";
+
+const GigaWeb = () => {
+  return (
+    <>
+      <Head>
+        <title>Gigaweb Agency - Abhi Dadhaniya</title>
+      </Head>
+      <Layout>
+        <GigaWebContent />
+      </Layout>
+    </>
+  );
+};
+
+export default GigaWeb;
